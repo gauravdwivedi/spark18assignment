45410035ee0c9617ab42632f2c16aa9b04b0a349
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-10-22 15:26:11 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import TaskList from "./TaskList";
+import "./App.css";
 
 function App() {
+  const [tasks, addTask] = useState([]);
+
+  const addTasks = (event) => {
+    addTask([
+      ...tasks,
+      {
+        id: tasks.length,
+        value: event.target.value,
+      },
+    ]);
+    console.log(tasks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input name="name" value={tasks.value} placeholder="Enter task" />
+        <button onClick={addTasks}>Add Task</button>
+        <div>
+          <ul>
+            {tasks.map((task) => (
+              <li key={task.id}>{task.value}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
